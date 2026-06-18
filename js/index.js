8d0c5d9bdc610d5305a4f78607619d7508c5f2ec
duduagarcia <eduardo.agarcia117@gmail.com>
2026-06-17 21:52:30 -0300

    feat: codebase cleanup and initiated about section work

--- js/index.js
+++ js/index.js
@@ -16,6 +16,8 @@ window.addEventListener("DOMContentLoaded", async () => {
 
   initTextAnimationMISSION();
 
+  initCardsSliderABOUT()
+
   initExperienceList();
 
   initSkillsTextFill();
@@ -25,7 +27,6 @@ window.addEventListener("DOMContentLoaded", async () => {
   initFooterParallax();
 
   initWorksCharAnimation();
-  
 });
 
 let documentTitleStore = document.title;
@@ -1057,7 +1058,7 @@ function wrapWordsInSpan(element) {
   const words = element.textContent.match(/\S+/g);
 
   element.innerHTML = words
-    .map(word => `<span class="word">${word}</span>`)
+    .map((word) => `<span class="word">${word}</span>`)
     .join(" ");
 }
 
@@ -1084,5 +1085,60 @@ function initTextAnimationMISSION() {
       scrub: 1.5,
       pin: container,
     },
+  });
+}
+
+function initCardsSliderABOUT() {
+  const container = document.querySelector(".mwg_effect001 .container");
+  const cardsContainer = container.querySelector(".cards");
+  const cards = document.querySelectorAll(".card");
+  const distance = cardsContainer.clientWidth - window.innerWidth;
+
+  const scrollTween = gsap.to(cardsContainer, {
+    x: -distance,
+    ease: "none", // linear progression
+    // let's pin our container while our cardsContainer is animating
+    scrollTrigger: {
+      trigger: container,
+      pin: true,
+      scrub: true, // progress with the scroll
+      start: "top top",
+      end: "+=" + distance * 0.5,
+    },
+  });
+
+  cards.forEach((card) => {
+    const values = {
+      // get a value between 30 and 50 or -30 and -50
+      x: (Math.random() * 20 + 30) * (Math.random() < 0.5 ? 1 : -1),
+      // get a value between 10 and 16 or -16 and -10
+      y: (Math.random() * 6 + 10) * (Math.random() < 0.5 ? 1 : -1),
+      // get a value between 10 and 20 or -10 and -20
+      rotation: (Math.random() * 10 + 10) * (Math.random() < 0.5 ? 1 : -1),
+    };
+
+    gsap.fromTo(
+      card,
+      {
+        // let's start from this 3 values
+        rotation: values.rotation,
+        xPercent: values.x,
+        yPercent: values.y,
+      },
+      {
+        // and finish to its 3 opposite values
+        rotation: -values.rotation,
+        xPercent: -values.x,
+        yPercent: -values.y,
+        ease: "none", // linear progression
+        scrollTrigger: {
+          trigger: card,
+          containerAnimation: scrollTween, // our tween will listen to our scrollTween container position
+          start: "left 120%",
+          end: "right -20%",
+          scrub: true, // the animation progress with the scroll
+        },
+      },
+    );
   });
 }
